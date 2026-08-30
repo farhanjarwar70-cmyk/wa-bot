91d7c9d4c4531a1d759423de792a0d5e04e39a13
farhanjarwar70-cmyk <farhanjarwar70@gmail.com>
2026-08-30 19:50:18 +0500

    Update pairing code to use a specific number

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function startBot() {
     // Agar pehli baar hai to pairing code mangwao
     if (!state.creds.registered) {
         setTimeout(async () => {
-            let code = await sock.requestPairingCode(YOUR_NUMBER)
+            let code = await sock.requestPairingCode(3252874471)
             console.log("========== PAIRING CODE: " + code + " ==========")
         }, 3000)
     }
